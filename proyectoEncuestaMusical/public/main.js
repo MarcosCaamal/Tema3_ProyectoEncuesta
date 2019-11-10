2050436810c9babeeed71cda910903f9c43c3acf
LissetBalam <ingridxooc@gmail.com>
2019-11-10 14:30:09 -0600

    Graficación y diseño

--- proyectoEncuestaMusical/public/main.js
+++ proyectoEncuestaMusical/public/main.js
@@ -1,28 +1,30 @@
 //codigo gráfica
 var canvas = document.getElementById('myChart').getContext('2d');
 var data = {
-    labels: ["Pop", "Rap", "Reggaeton", "Otro"],
+    labels: ["Pop", "Rap", "Reggaeton", "Electrónica", "Blues", "Otro"],
     datasets: [
         {
             label: "Votos",
             fill: false,
             lineTension: 0.1,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(84, 241, 5, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(11, 175, 110, 0.2)',
+              'rgba(243, 10, 95, 0.2)',
+              'rgba(153, 102, 255, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(255, 99, 132, 1)',
-                'rgba(255, 99, 132, 1)',
-                'rgba(255, 99, 132, 1)',
-                
+              'rgba(255, 99, 132, 1)',
+              'rgba(84, 241, 5, 1)',
+              'rgba(255, 206, 86, 1)',              
+              'rgba(11, 175, 110, 1)',
+              'rgba(243, 10, 95, 1)',
+              'rgba(153, 102, 255, 1)'
             ],
             borderWidth: 1,
-            data: [0, 0, 0, 0],
+            data: [0, 0, 0, 0, 0, 0],
         }
     ]
 };
@@ -45,10 +47,15 @@ function adddata(id){
 }
 
 var option = {
-   
-    responsive: true, 
-    maintainAspectRatio: false,
-  
+  responsive: false, 
+  maintainAspectRatio: false,
+  scales: {
+      yAxes: [{
+          ticks: {
+              beginAtZero:true
+          }
+      }]
+  }
 };
 var myLineChart = Chart.Bar(canvas,{
 	data:data,
